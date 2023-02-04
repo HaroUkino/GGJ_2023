a32fcbd97fb4e80c18617168b8fc17caefbbc546
pedroHenriqueBugaboo <pedro.henrieque@bugaboostudio.com>
2023-02-04 16:15:27 -0300

    rounded player position on player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,25 +11,25 @@ public class PlayerMovement : MonoBehaviour, IMovable {
     public float MoveSpeed => moveSpeed;
 
     Vector2Int _dir;
-    SpriteRenderer _sprRenderer;
-    Vector3 _target;
+    Vector3Int _target;
     bool _moving;
 
     void Awake() {
         transform.position = new Vector2( Mathf.RoundToInt( transform.position.x ), Mathf.RoundToInt( transform.position.y ) );
-        _sprRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
         if ( ValidateMovement() && !_moving ) {
             _moving = true;
-            _target = transform.position + new Vector3( _dir.x, _dir.y );
+            _target =  Vector3Int.RoundToInt( transform.position ) + new Vector3Int( _dir.x, _dir.y );
         }
         if( _moving ) {
             if ( Vector3.Distance( transform.position, _target ) > .01f )
                 transform.position = Vector3.MoveTowards( transform.position, _target, Time.deltaTime * moveSpeed );
-            else
+            else {
+                transform.position = _target;
                 _moving = false;
+            }
         }
     }
 
@@ -39,14 +39,12 @@ public class PlayerMovement : MonoBehaviour, IMovable {
     }
 
     private void OnDrawGizmos() {
-        if ( _sprRenderer == null ) return;
-        var extend = _sprRenderer.bounds.extents;
         Gizmos.color = Color.green;
         Gizmos.DrawRay( transform.position, new Vector2( _dir.x, _dir.y ) * rayCastDist );
     }
 
     public void Move( Vector2 dir ) {
-        Vector2Int newDir = new Vector2Int( Mathf.RoundToInt( dir.x ), Mathf.RoundToInt( dir.y ) );
+        Vector2Int newDir = Vector2Int.RoundToInt( dir );
         if ( Mathf.Abs( newDir.x ) + Mathf.Abs( newDir.y ) > 1 ) {
             if ( directionOverride )
                 newDir = new Vector2Int( Mathf.Abs( _dir.x ) == 1 ? 0 : newDir.x, Mathf.Abs( _dir.y ) == 1 ? 0 : newDir.y );
